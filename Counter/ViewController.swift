54ad2b6d999b41a857ff10e28140d509a98345ac
DmitriyK <dibaker.man@yandex.ru>
2024-06-13 12:10:00 +0300

    Проект по подсчету количества кликов

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var buttonPress: UIButton!
+    
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        nameLabel.text = "Счётчик:"
+        
+        counterLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 64, weight: .medium)
+        counterLabel.textColor = .systemBlue
+        
+        buttonPress.setTitle("Нажми меня", for: .normal)
+        buttonPress.setTitleColor(.white, for: .normal)
+        buttonPress.backgroundColor = .systemBlue
+        buttonPress.layer.cornerRadius = 10
+        
+        
+        updateCounterLabel()
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        counter += 1
+        updateCounterLabel()
+    }
+    
+    func updateCounterLabel() {
+        counterLabel.text = "\(counter)"
+    }
 }
 
